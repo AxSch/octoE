b5dd3140ab81c091d851b88d00f0efeb43ff6ea2
AxSch <alex_schuneman@outlook.com>
2021-01-23 22:34:11 +0000

    Import & utilise Header & ProductContainer

--- octopus/client/src/App.jsx
+++ octopus/client/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Header from './Components/Product/Header/Header';
+import { ProductContainer } from './Containers/Product/Product';
 
 const App = () => {
     return (
-       <h1>Hello World</h1>
+        <>
+        <Header/>
+        <ProductContainer />
+        </>
     )
 };
 
