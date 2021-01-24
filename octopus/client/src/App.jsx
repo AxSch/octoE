c6bd12985e75ef3860ee5cd35d4a722918b45bae
AxSch <alex_schuneman@outlook.com>
2021-01-24 03:30:44 +0000

    Correct import

--- octopus/client/src/App.jsx
+++ octopus/client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './Components/Product/Header/Header';
-import { ProductPage } from './Containers/Product/Product';
+import { ProductPage } from './Pages/Product/Product';
 
 const App = () => {
     return (
