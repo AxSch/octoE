529fc181280f9074cf99292f5d43681ef6e56351
AxSch <alex_schuneman@outlook.com>
2021-01-25 01:31:38 +0000

    Refactor storeItems() & selectCartItems()

--- octopus/client/src/reducers/cart/cartSlice.js
+++ octopus/client/src/reducers/cart/cartSlice.js
@@ -7,13 +7,37 @@ const cartSlice = createSlice({
     },
     reducers: {
         storeItems: (state, action) => {
-            state.items = {
-                ...state.items,
-                [action.payload.id]: action.payload
+            if (state.items && Object.values(state.items).length !== 0) {
+                const quantity = state.items[action.payload.id].quantity
+                state.items = {
+                    ...state.items,
+                    [action.payload.id]: {
+                        ...action.payload,
+                        quantity: action.payload.quantity + quantity
+                    }
+                }
+
+            } else {
+                state.items = {
+                    ...state.items,
+                    [action.payload.id]: {
+                        ...action.payload,
+                    }
+                }
             }
         },
     }
 })
+
+export const selectCartItems = state => {
+    let numberOfItems = 0
+    if (state.cart.items && Object.values(state.cart.items).length !== 0) {
+        Object.values(state.cart.items).forEach(item => {
+            numberOfItems += item.quantity
+        });
+    }
+    return numberOfItems
+}
 
 export const { storeItems } = cartSlice.actions
 
